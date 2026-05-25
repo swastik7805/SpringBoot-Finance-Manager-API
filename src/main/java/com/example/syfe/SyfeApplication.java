279fb6eeb117bd65435b186aeeb0a4410c0c6fca
swastik7805 <whirphool123@gmail.com>
2026-05-25 12:06:29 +0530

    feat: Setup springboot project

---
+++ src/main/java/com/example/syfe/SyfeApplication.java
@@ -0,0 +1,13 @@
+package com.example.syfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyfeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyfeApplication.class, args);
+    }
+
+}
